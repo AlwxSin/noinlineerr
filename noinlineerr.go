1a74ee6488c79f51ff6740b7e6a9f4b31dce1f21
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-05-23 17:27:27 +0200

    feat: use inspector

--- noinlineerr.go
+++ noinlineerr.go
@@ -5,56 +5,61 @@ import (
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
-type analyzer struct{}
-
 func NewAnalyzer() *analysis.Analyzer {
-	a := &analyzer{}
-
 	return &analysis.Analyzer{
-		Name: "noinlineerr",
-		Doc:  "Disallows inline error handling using `if err := ...; err != nil`",
-		Run:  a.run,
+		Name:     "noinlineerr",
+		Doc:      "Disallows inline error handling (`if err := ...; err != nil {`)",
+		Run:      run,
+		Requires: []*analysis.Analyzer{inspect.Analyzer},
 	}
 }
 
-func (a *analyzer) run(pass *analysis.Pass) (any, error) {
-	for _, file := range pass.Files {
-		ast.Inspect(file, func(n ast.Node) bool {
-			// look for if statements with an Init clause
-			ifStmt, ok := n.(*ast.IfStmt)
-			if !ok || ifStmt.Init == nil {
-				return true
-			}
-
-			// check if the init clause is an assignment
-			assignStmt, ok := ifStmt.Init.(*ast.AssignStmt)
-			if !ok {
-				return true
-			}
-
-			// iterate over left-hand side variables of the assignment
-			for _, lhs := range assignStmt.Lhs {
-				ident, ok := lhs.(*ast.Ident)
-				if !ok {
-					continue
-				}
-
-				// confirm type is error
-				obj := pass.TypesInfo.ObjectOf(ident)
-				if obj == nil || !strings.Contains(obj.Type().String(), "error") {
-					continue
-				}
-
-				// report usage of inline error assignment
-				pass.Reportf(
-					ident.Pos(),
-					"avoid inline error handling using `if err := ...; err != nil`; use plain assignment `err := ...`",
-				)
-			}
-			return true
-		})
+func run(pass *analysis.Pass) (any, error) {
+	insp, ok := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	if !ok {
+		return nil, nil //nolint:nilnil // nothing to return
 	}
+
+	nodeFilter := []ast.Node{
+		(*ast.IfStmt)(nil),
+	}
+
+	insp.Preorder(nodeFilter, func(n ast.Node) {
+		ifStmt, ok := n.(*ast.IfStmt)
+		if !ok || ifStmt.Init == nil {
+			return
+		}
+
+		// check if the init clause is an assignment
+		assignStmt, ok := ifStmt.Init.(*ast.AssignStmt)
+		if !ok {
+			return
+		}
+
+		// iterate over left-hand side variables of the assignment
+		for _, lhs := range assignStmt.Lhs {
+			ident, ok := lhs.(*ast.Ident)
+			if !ok {
+				continue
+			}
+
+			// confirm type is error
+			obj := pass.TypesInfo.ObjectOf(ident)
+			if obj == nil || !strings.Contains(obj.Type().String(), "error") {
+				continue
+			}
+
+			// report usage of inline error assignment
+			pass.Reportf(
+				ident.Pos(),
+				"avoid inline error handling using `if err := ...; err != nil`; use plain assignment `err := ...`",
+			)
+		}
+	})
+
 	return nil, nil //nolint:nilnil // nothing to return
 }
